ae220b4daa200a75cdac303a0b3ba6b54cbf5cf2
Jonathan Slipak <seriedigital@gmail.com>
2021-06-25 00:22:27 -0300

    Posible solucion

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,75 +1,47 @@
-import React, {useState} from 'react'
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import ItemDetailCard from './ItemDetailCard'
 
 const useStyles = makeStyles({
+  contador: {
+    marginTop: 20,
+  },
+  btnAddToCar: {
+    marginBottom: 20,
+  },
+});
 
-    contador:{
-      marginTop: 20,
-    },
-    btnAddToCar: {
-        marginBottom: 20
-    }
-  
-  });
+const Contador = (props) => {
+  const classes = useStyles();
 
+  return (
+    <>
+      <Grid className={classes.contador}>
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <Button onClick={() => props.onAdd('suma')}> + </Button>
+          <Button onClick={() => props.onAdd('resta')}> - </Button>
+        </ButtonGroup>
+        <p>{`Candtidad ${props.clicks}`} </p>
+      </Grid>
 
-const useContador = () => {
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.btnAddToCar}
+        >
+          Agregar al carrito
+        </Button>
+        <Typography />
+        <Button variant="contained" color="primary">
+          {' '}
+          Terminar Compra
+        </Button>
+      </div>
+    </>
+  );
+};
 
-
-    const [clicks, setClicks] = useState(1)
-    const stock = 9
-
-    const aumentarContador = () =>{
-        if (clicks <= stock) {
-            setClicks(clicks + 1)
-        }
-        else{
-            Button.disabled = false
-            alert("Ups! Tenemos solo 10 de estos productos en stock")
-        }
-    }
-
-    const restarContador = () =>{
-        if (clicks > 1) setClicks(clicks -1)
-    }
-    return {clicks, aumentarContador, restarContador}
-}
-
-
-
-const Contador = () =>  {
-
-    const classes = useStyles();
-
-    const {clicks, aumentarContador, restarContador} = useContador(0)
-
-    return (
-        <>
-        <Grid className={classes.contador}>
-            <ButtonGroup disableElevation variant="contained" color="primary">
-                <Button onClick={aumentarContador}> + </Button>
-                <Button onClick={restarContador}> - </Button>
-            </ButtonGroup>
-            <p>{`Candtidad ${clicks}` }  </p>
-
-        </Grid>
-            
-        <div>
-            
-            <Button variant="contained" color="primary" className={classes.btnAddToCar}>
-            Agregar al carrito</Button>
-            <Typography/>
-            <Button variant="contained" color="primary"> Terminar Compra</Button>
-            
-        </div>
-        </>
-    )
-}
-
-
-export default Contador
+export default Contador;
--- src/components/ItemDetailCard.jsx
+++ src/components/ItemDetailCard.jsx
@@ -1,117 +1,109 @@
-
-import React ,{useEffect, useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {BASE_URL} from '../constants';
-import {useParams} from 'react-router-dom'
+import { BASE_URL } from '../constants';
+import { useParams } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import Contador from './../components/Contador'
-import {Divider} from '@material-ui/core'
+import Contador from './../components/Contador';
+import { Divider } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
-   position: 'relative',
-   left: 100,
-   display: 'flex',
-   alignItems: 'center',
- 
+    position: 'relative',
+    left: 100,
+    display: 'flex',
+    alignItems: 'center',
   },
   media: {
     height: 200,
     width: 200,
     paddingLeft: 140,
-    
-    
   },
-  divider:{
+  divider: {
     marginTop: 20,
-    
   },
-  cardContent:{
+  cardContent: {
     width: 100,
-  }
-  
-
+  },
 });
 
 const ItemDetailCard = (props) => {
+  const [dataDetail, setProductos] = useState([]);
+  const [click, setClick] = useState(0);
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    const [dataDetail, setProductos] = useState([])
-
-    let id = useParams().id; 
-    let title = useParams().title;
-    console.log(id, title)
-
-   
-
-
-    const onAdd = (quantityToAdd) => {
-      //hemos recibido un evento
+  const fnClick = (str) => {
+    if (str === 'suma') {
+      return setClick(click + 1);
+    } else if (str === 'resta') {
+      click > 0 ? setClick(click - 1) : console.log('error');
     }
+  };
 
-    const fetchData = async () => {
-        
-        try{
-            const res = await fetch(`${BASE_URL}/sites/MLA/search?q=${title}&FilterID=${id}`)//URL aqui. 
-            const datos = await res.json()
-            setProductos(datos.results[0])
-            
-        }catch (error){
-            console.log(error)
-        }
+  let id = useParams().id;
+  let title = useParams().title;
+  console.log(id, title);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `${BASE_URL}/sites/MLA/search?q=${title}&FilterID=${id}`,
+      ); //URL aqui.
+      const datos = await res.json();
+      setProductos(datos.results[0]);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        console.log('useEffect')
-        fetchData()
-    },[3000])
+  useEffect(() => {
+    console.log('useEffect');
+    fetchData();
+  }, [3000]);
 
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          component="img"
+          alt="ProductoDetalle"
+          heigth="400"
+          image={dataDetail.thumbnail}
+          title={dataDetail.title}
+        />
+      </CardActionArea>
+      <CardContent classes={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {dataDetail.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="H2">
+          Precio: {dataDetail.price}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="h2">
+          Stock: {dataDetail.available_quantity}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Condicion: {dataDetail.condition}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Cantidad Vendida: {dataDetail.sold_quantity}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+        ></Typography>
 
-    return  (
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia className={classes.media}
-            component="img"
-            alt="ProductoDetalle"
-            heigth="400"
-            image={dataDetail.thumbnail}
-            title=    {dataDetail.title}
-          />
-          </CardActionArea >
-          <CardContent classes={classes.cardContent}>
-            <Typography gutterBottom variant="h5" component="h2">
-            {dataDetail.title}
-             
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="H2">
-             Precio: {dataDetail.price}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="h2">
-              Stock: {dataDetail.available_quantity}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Condicion: {dataDetail.condition}
-            </Typography>
-             <Typography variant="body2" color="textSecondary" component="p">
-              Cantidad Vendida: {dataDetail.sold_quantity}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-            </Typography>
+        <Divider className={classes.divider} />
 
-            <Divider className={classes.divider}/>  
+        <Contador onAdd={fnClick} clicks={click} />
+      </CardContent>
+    </Card>
+  );
+};
 
-            <Contador onConfirm={onAdd} />  
-
-          </CardContent>
-
-       
-      </Card>
-    );
-  }
-
-export default ItemDetailCard
+export default ItemDetailCard;
